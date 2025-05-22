40eb3bda0728eabfe8ecfe6d528f797b05829545
Gianco99 <gianfrancocyclohexane@gmail.com>
2025-05-22 13:35:03 +0200

    Updated analyzers

--- ZH_analyzer.h
+++ ZH_analyzer.h
@@ -16,6 +16,7 @@ class user : public AnalyzerBase
       MAbool Initialize(const MA5::Configuration& cfg, const std::map<std::string,std::string>& parameters);
       void Finalize(const SampleFormat& summary, const std::vector<SampleFormat>& files);
       MAbool Execute(SampleFormat& sample, const EventFormat& event);
+      void FillHistoAllRegions(const std::string &baseName, double value, const std::vector<std::string> &regions);
 
   private : 
 };
